24241f5e481c6a6d992d3aa48db96dc1da068c17
russellborja <russellborja@gmail.com>
2018-03-29 21:55:52 -0400

    Clean up error handling and responses. Add endpoints to get all users and therapists

--- app.py
+++ app.py
@@ -10,23 +10,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db.init_app(app)
 heroku = Heroku(app)
 
-class Error(Exception):
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
+VERSION = "v1"
 
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv["message"] = "ERROR: %s" % self.message
-        return rv
+def bad_request(message):
+    response = jsonify({'error': message})
+    response.status_code = 400
+    return response
 
-@app.errorhandler(Error)
-def handle_error(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
+def not_found(message):
+    response = jsonify({'error': message})
+    response.status_code = 404
     return response
 
 # Set "homepage" to index.html
@@ -34,118 +27,129 @@ def handle_error(error):
 def index():
     return render_template("index.html")
 
-@app.route("/api/v1/users/", methods=["POST"])
+@app.route("/api/{version}/users/".format(version=VERSION), methods=["POST"])
 def add_user():
     body = request.get_json()
-
     email = body.get("email")
     username = body.get("username")
 
-    if not username or not email:
-        raise Error("Username and e-mail must be provided", 400)
+    if not body or not username or not email:
+        return bad_request("Username and e-mail must be provided")
 
     if not User.query.filter_by(username=username).count():
         user = User(body)
-        try:
-            db.session.add(user)
-            db.session.commit()
-        except Exception as e:
-            raise Error(e, 400)
-        return jsonify({"message": "Succesfully added user."}), 201
+        db.session.add(user)
+        db.session.commit()
+        user_added = UserSchema().dump(user)
+        return jsonify(user_added.data), 201
     else:
-        raise Error("User already in database", 400)
+        return bad_request("User already in database")
 
-@app.route("/api/v1/users/<int:id>", methods=["PUT"])
+@app.route("/api/{version}/users/<int:id>".format(version=VERSION), methods=["PUT"])
 def update_user(id):
     body = request.get_json()
-
     email = body.get("email")
     username = body.get("username")
 
     if not username or not email:
-        raise Error("Username and e-mail must be provided", 400)
+        return bad_request("Username and e-mail must be provided")
+
+    if User.query.filter_by(username=username).count():
+        return bad_request("Username already exists, select another username")
     try:
         user = User.query.get(id)
         user.email = email
         user.username = username
-    except Exception as e:
-        raise Error("Could not find therapist with given ID", 404)
 
-    try:
+        user_updated = UserSchema().dump(user)
         db.session.commit()
-    except Exception as e:
-        raise Error(e, 400)
-    return jsonify(user), 201
+        return jsonify(user_updated.data), 201
+    except:
+        return not_found("Could not find user with given ID")
 
-@app.route("/api/v1/users/<int:id>/", methods=["GET"])
+@app.route("/api/{version}/users".format(version=VERSION), methods=["GET"])
+def get_users():
+    response = {
+        "users": []
+    }
+    users = User.query.all()
+    for user in users:
+        user_obj = UserSchema().dump(user)
+        response["users"].append(user_obj.data)
+    return jsonify(response)
+
+@app.route("/api/{version}/users/<int:id>/".format(version=VERSION), methods=["GET"])
 def get_user(id):
     try:
-        user = User.query.filter_by(user_id=id).one()
-    except Exception as e:
-        raise Error("Could not find user with given ID", 404)
-    user_result = UserSchema().dump(user)
+        user = User.query.filter_by(user_id=id).first_or_404()
+        user_result = UserSchema().dump(user)
+        return jsonify(user_result.data)
+    except:
+        return not_found("Could not find user with given ID")
 
-    if user_result.errors:
-        raise Error("Encountered errors retrieving user", 500, user_result.errors)
-    return jsonify(user_result.data), 200
-
-@app.route("/api/v1/therapists/", methods=["POST"])
+@app.route("/api/{version}/therapists/".format(version=VERSION), methods=["POST"])
 def add_therapist():
     body = request.get_json()
-
     name = body.get("name")
     location = body.get("location")
     specialty = body.get("specialty")
 
-    if not name or not specialty or not location:
-        raise Error("Name, location, and specialty must be provided", 400)
+    if not body or not name or not specialty or not location:
+        return bad_request("Name, location, and specialty must be provided")
 
     if not Therapist.query.filter_by(name=name).count():
         therapist = Therapist(body)
-        try:
-            db.session.add(therapist)
-            db.session.commit()
-        except Exception as e:
-            raise Error(e, 400)
-        return jsonify(therapist), 201
+        db.session.add(therapist)
+        db.session.commit()
+        therapist_added = TherapistSchema().dump(therapist)
+        return jsonify(therapist_added.data), 201
     else:
-        raise Error("Therapist already in database", 400)
+        return bad_request("Therapist already in database")
 
-@app.route("/api/v1/therapists/<int:id>", methods=["PUT"])
+@app.route("/api/{version}/therapists/<int:id>".format(version=VERSION), methods=["PUT"])
 def update_therapist(id):
     body = request.get_json()
-
     name = body.get("name")
     location = body.get("location")
     specialty = body.get("specialty")
 
     if not name or not specialty or not location:
-        raise Error("Name, location, and specialty must be provided", 400)
+        return bad_request("Name, location, and specialty must be provided")
+
+    if Therapist.query.filter_by(name=name).count():
+        return bad_request("Therapist already exists, select another name")
+
     try:
         therapist = Therapist.query.get(id)
         therapist.name = name
         therapist.location = location
         therapist.specialty = specialty
-    except Exception as e:
-        raise Error("Could not find therapist with given ID", 404)
 
-    try:
+        therapist_updated = TherapistSchema().dump(therapist)
         db.session.commit()
-    except Exception as e:
-        raise Error(e, 400)
-    return jsonify(therapist), 201
+        return jsonify(therapist_updated.data), 201
+    except:
+        return not_found("Could not find therapist with given ID")
 
-@app.route("/api/v1/therapists/<int:id>", methods=["GET"])
+@app.route("/api/{version}/therapists/".format(version=VERSION), methods=["GET"])
+def get_therapists():
+    response = {
+        "therapists": []
+    }
+    therapists = Therapist.query.all()
+    for therapist in therapists:
+        therapist_obj = TherapistSchema().dump(therapist)
+        response["therapists"].append(therapist_obj.data)
+    return jsonify(response)
+
+@app.route("/api/{version}/therapists/<int:id>".format(version=VERSION), methods=["GET"])
 def get_therapist(id):
     try:
-        therapist = Therapist.query.filter_by(therapist_id=id).one()
-    except Exception as e:
-        raise Error("Could not find therapist with given ID", 404)
-    therapist_result = TherapistSchema().dump(therapist)
-
-    if therapist_result.errors:
-        raise Error("Encountered errors retrieving user", 500, therapist_result.errors)
-    return jsonify(therapist_result.data), 200
+        therapist = Therapist.query.filter_by(therapist_id=id).first_or_404()
+        therapist_result = TherapistSchema().dump(therapist)
+        return jsonify(therapist_result.data)
+    except:
+        return not_found("Could not find therapist with given ID")
     
 
 if __name__ == "__main__":
